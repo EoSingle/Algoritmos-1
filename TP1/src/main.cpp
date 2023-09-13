9d7aa349198159305e4d8cfc4384157fbe2b852f
EoSingle <olive.albano@gmail.com>
2023-09-13 09:20:00 -0300

    Versão 13-09/1

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -3,32 +3,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <math.h>
-
+#include <cmath>
+#include <stack>
+#include <queue>
 #include <algorithm>
+
+#include <map>
+#include <set>
 
 // Estrutura ponto
 struct Ponto{
     float x,y;
-};
-struct Aresta{
-    unsigned int destino;
-    int cor;
-
-    Aresta(){
-        this->cor = 0;
-    }
-
-    bool visitado(){
-        return this->cor != 0;
-    }
 };
 
 // Estrutura que representa um vertice
 struct Vertice{
     Ponto cord; 
     int grau;
-    std::vector<Aresta> vizinhos;
+    std::vector<unsigned int> vizinhos;
 };
 
 // Distancia euclidiana de a para b.
@@ -64,23 +56,28 @@ bool Compara(Ponto base, Ponto a, Ponto b){
     return angulo_a > angulo_b;
 }
 
-// Visita todos as arestas e as pinta de preto (1) - DFS
-void encontraFaces(std::vector<std::vector<Aresta>>& arestas, int vertice, unsigned int anterior = 0){
-    for(unsigned int i=0; i < arestas[vertice-1].size(); i++){
-        if(!arestas[vertice-1][i].visitado() && arestas[vertice-1][i].destino != anterior){
-            arestas[vertice-1][i].cor = 1;
-            std::cout << "Aresta " << vertice << " -> " << arestas[vertice-1][i].destino << std::endl;
-            encontraFaces(arestas, arestas[vertice-1][i].destino, vertice);
-        }
-    }
+bool ComparaHora(Ponto base, Ponto a, Ponto b){
+    int orientation = TipoCurva(base, a, b);
+
+    // Desempate pelo mais distante
+    if(orientation == 0) 
+        return (Distancia(base, b) >= Distancia(base, a))? 0 : 1;
+
+    // True para sentido anti-horário
+    return (orientation == 1)? 1 : 0;
 }
 
+// Visita todos as arestas e as pinta de preto (1) - DFS
+void encontraFaces(){
+}
 
 int main(int argc, char* argv[]){
     // Vetor de vertices
     std::vector<Vertice> vertices;
     // Numero de vértices e arestas
     int nvertices, narestas;
+    // Vizinhos de cada vertice ordenado
+    std::vector<std::queue<unsigned int>> adjacentes;
 
     std::cin >> nvertices >> narestas;
     vertices.resize(nvertices);
@@ -89,15 +86,22 @@ int main(int argc, char* argv[]){
         std::cin >> vertices[i].cord.x >> vertices[i].cord.y >> vertices[i].grau;
         vertices[i].vizinhos.resize(vertices[i].grau);
         for(int j = 0; j < vertices[i].grau; j++){
-            std::cin >> vertices[i].vizinhos[j].destino;
+            std::cin >> vertices[i].vizinhos[j];
         }
     }
 
     // Ordena os vizinhos de cada vértice
     for(Vertice& vertice: vertices){
-        std::sort(vertice.vizinhos.begin(), vertice.vizinhos.end(), [&](Aresta a, Aresta b){
-            return Compara(vertice.cord, vertices[a.destino-1].cord, vertices[b.destino-1].cord);
+        std::sort(vertice.vizinhos.begin(), vertice.vizinhos.end(), [&](int a, int b){
+            return Compara(vertice.cord, vertices[a-1].cord, vertices[b-1].cord);
         });
+    }
+
+    // Insere os vértices de forma ordenada na fila
+    for(int i=0; i < nvertices; i++){
+        for(int j=0; j<vertices[i].grau; j++){
+            adjacentes[i].push(vertices[i].vizinhos[j]);
+        }
     }
 
     // Imprime os vertices
@@ -106,45 +110,19 @@ int main(int argc, char* argv[]){
         std::cout << "Grau: " << vertices[i].grau << std::endl;
         std::cout << "Vizinhos: ";
         for(int j=0; j < vertices[i].grau; j++){
-            std::cout << vertices[i].vizinhos[j].destino << " ";
+            std::cout << vertices[i].vizinhos[j] << " ";
         }
         std::cout << std::endl << std::endl;
     }
 
-    // Extrai as arestas
-    std::vector<std::vector<Aresta>> arestas;
-    arestas.resize(nvertices);
-    for(unsigned int i=0; i < vertices.size(); i++){
-        for(int j=0; j < vertices[i].grau; j++){
-            arestas[i].push_back(vertices[i].vizinhos[j]);
-        }
-    }
-
-    // Imprime as arestas
-    for(unsigned int i=0; i < arestas.size(); i++){
-        std::cout << "Arestas do vertice " << i + 1 << ": ";
-        for(unsigned int j=0; j < arestas[i].size(); j++){
-            std::cout << arestas[i][j].destino << " ";
-        }
-        std::cout << std::endl;
-    }
-    
     // Encontra as faces
-    encontraFaces(arestas, 1);
+    encontraFaces();
 
     return 0;
 }
 
 /*
-Ideia para o algoritmo
-
-Considerar os dois caminhos de cada aresta (OK)
-
-Ordenar os vizinhos de cada vértice pelo angulo no sentido antihorário (OK)
-
-Andar por cada aresta em sentido antihorário e colorir ela ao cruzá-la 
-
-Ao voltar para o vértice original a face foi encontrada
-
-Fazer isso até colorir todas as arestas
+vector<Vertices>
+map<ID,pos>
+set<pos,compara>
 */
